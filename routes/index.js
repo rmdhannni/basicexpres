3092bb0f46f1fccf367ae170a3b7589e0d45802c
rmdhannni <ggdani137@gmail.com>
2024-02-22 11:25:13 +0700

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var connection = require('../config/database.js');
 
-router.get('/', function (req, res, next) {
-    connection.query('select * from kategori order by id_kategori desc', function (err, rows) {
-        if (err) {
-            req.flash('error', err);
-            res.render('kategori/index', {
-                error: err
-            });
-        } else {
-            res.render('kategori/index', {
-                data: rows
-            });
-        }
-    });
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
+
 
 module.exports = router;
--- routes/kategori.js
+++ routes/kategori.js
@@ -1,18 +1,42 @@
+
 var express = require('express');
 var router = express.Router();
-
 var connection = require('../config/database.js');
 
-router.get('/', function(req, res, next){
-    connection.query('select * from kategori order by id_kategori desc', function(err, rows){
-        if(err){
-            req.flash('error',err);
-        }else{
-            res.render('kategori/index',{
+router.get('/', function (req, res, next) {
+    connection.query('select * from kategori order by id_kategori desc', function (err, rows) {
+        if (err) {
+            req.flash('error', err);
+            res.render('kategori/index', {
+                error: err
+            });
+        } else {
+            res.render('kategori/index', {
                 data: rows
             });
         }
     });
 });
+
+router.post('/store', (req, res, next) => {
+    try{
+        let {nama_kategori} = req.body;
+        let Data = {
+            nama_kategori
+        };
+        connection.query('insert into kategori set ?', Data, function(err, result){
+            if(err){
+                req.flash('error', 'Gagal menyimpan data ke database');
+            }else{
+                req.flash('success', 'Berhasil menyimpan data!');
+            }
+            res.redirect('/kategori');
+        });
+    }catch{
+        req.flash('error','Terjadi kesalahan pada fungsi');
+        res.redirect('/kategori');
+    }
+})
+
 
 module.exports = router;
